96d3fc9f68d5bfb070e9985222efc34a1b000121
Michael Doyle <mvdoyle@uab.edu>
2018-01-29 10:26:57 -0600

    cleaned up white space and refactored AskBoolQuestion Method

--- OnBoardingApp/Program.cs
+++ OnBoardingApp/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OnBoardingApp
 {
@@ -23,9 +19,10 @@ namespace OnBoardingApp
             user.PinNumber = AskIntQuestion("What is the 4 digit numeric Pin Number for the Account?", 4);
             Console.WriteLine($"Awesome! Thanks for using the onboarding application, {user.FullName}! Your information has been stored!\r\n");
 
-            Console.WriteLine($" Name:{user.FullName}\r\n Account Owner:{user.IsAccountOwner}\r\n Pin Number:{user.PinNumber}");
+            Console.WriteLine($"Name:{user.FullName}");
+            Console.WriteLine($"Account Owner:{user.IsAccountOwner}");
+            Console.WriteLine($"Pin Number:{ user.PinNumber}");
             Console.ReadLine();
-            
         }
 
         /// <summary>
@@ -46,12 +43,19 @@ namespace OnBoardingApp
         /// <returns></returns>
         public static bool AskBoolQuestion(string question)
         {
-            var response = AskQuestion(question + " yes/no");
-            if (response.ToLower().Contains("yes"))
+            while (true)
             {
-                return true;
+                var response = AskQuestion(question + " yes/no");
+                if (response.ToLower().Contains("yes"))
+                {
+                    return true;
+                }
+
+                if (response.ToLower().Contains("no"))
+                {
+                    return false;
+                }
             }
-            else return false;
         }
 
         /// <summary>
@@ -62,23 +66,20 @@ namespace OnBoardingApp
         /// <returns></returns>
         public static int AskIntQuestion(string question, int length = 0)
         {
-            var response = AskQuestion(question);
-            if (length > 0 && length != response.Length)
+            while (true)
             {
-                Console.WriteLine("Invalid entry - response should be a 4 digit value");
-                return AskIntQuestion(question,4);
-            }
+                var response = AskQuestion(question);
+                if (length > 0 && length != response.Length)
+                {
+                    Console.WriteLine("Invalid entry - response should be a 4 digit value");
+                    continue;
+                }
 
-
-            var value = 0;
-            if (int.TryParse(response, out value))
-            {
-                return value;
-            }
-            else
-            {
+                if (int.TryParse(response, out var value))
+                {
+                    return value;
+                }
                 Console.WriteLine("Invalid entry - response should be a numeric value");
-                return AskIntQuestion(question, 4);
             }
         }
     }
